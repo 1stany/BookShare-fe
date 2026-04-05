8307133773d10845bc034f5443a5be5d1020c7cb
Stanislao Tariffa <stany.tariffa@gmail.com>
2026-04-05 21:53:56 +0200

    item

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -8,21 +8,33 @@ import { Item } from '../model/item.model';
   standalone: true,
   imports: [RouterModule],
   templateUrl: './item.component.html',
-  styleUrl: './item.component.css'
+  styleUrl: './item.component.css',
 })
+export class ItemComponent implements OnInit {
+  item: Item | undefined;
 
-export class ItemComponent implements OnInit{
-
-  item : Item | undefined;
-
-  constructor(private itemService : ItemService, private route : ActivatedRoute) { }
-  
+  constructor(
+    private itemService: ItemService,
+    private route: ActivatedRoute,
+  ) {}
 
   ngOnInit(): void {
-    const id = this.route.snapshot.paramMap.get("id");
-    if(id){
-      this.itemService.getItem(+id).subscribe(i => this.item = i);
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.itemService.getItem(+id).subscribe((i) => {
+        if (i && i.cover) {
+          // Puliamo il percorso come già stavi facendo
+          const cleanPath = i.cover.startsWith('/')
+            ? i.cover.substring(1)
+            : i.cover;
+
+          // Aggiungiamo un parametro casuale (timestamp) per distruggere la cache
+          const t = new Date().getTime();
+          i.cover = `http://localhost:8080/${cleanPath}?v=${t}`;
+        }
+        this.item = i;
+        console.log('URL immagine finale:', this.item?.cover);
+      });
     }
   }
-
 }
